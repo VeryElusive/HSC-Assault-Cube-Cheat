6a95340057b91aabca6645f1acbbfad24d42cf51
IAmCheck <artieleiperalt@gmail.com>
2023-01-10 18:53:20 +1100

    first

---
+++ base/main.cpp
@@ -0,0 +1,26 @@
+#include <thread>
+#include <Windows.h>
+
+void Entry( HMODULE hModule ) {
+	while ( true ) {
+		if ( GetAsyncKeyState( VK_F11 ) )
+			break;
+
+
+		std::this_thread::sleep_for( std::chrono::milliseconds( 200 ) );
+	}
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD dwReason, LPVOID lpReserved ) {
+	if ( dwReason == DLL_PROCESS_ATTACH ) {
+		// no need for DLL_THREAD_ATTACH and DLL_THREAD_DETACH calls
+		DisableThreadLibraryCalls( hModule );
+
+		// once the cheat is uninjected we want to close the windows handle for our cheat process.
+		CloseHandle( CreateThread( 0, 0, LPTHREAD_START_ROUTINE( Entry ), hModule, 0, 0 ) );
+
+		return TRUE;
+	}
+
+	return FALSE;
+}
